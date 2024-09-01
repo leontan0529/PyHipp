b20676a940b493b943948b13d79f90f50d188e08
AWS ParallelCluster user <ec2-user@ip-10-0-8-56.ap-southeast-1.compute.internal>
2024-09-01 02:59:14 +0000

    new changes

---
+++ ec2snapshot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@10.0.8.56 "source ~/.bash_profile; pcluster update-compute-fleet --status STOP_REQUESTED -n MyCluster01; ~/update_snapshot.sh data 2 MyCluster01"
